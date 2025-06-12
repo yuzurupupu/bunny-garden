6b592b2b1ba044e6116af24d37dcccba84f26116
yuzurupupu <2543022@dundee.ac.uk>
2025-06-12 15:32:32 +0800

    feature: axe tutorial done

--- scenes/objects/rock/rock.gd
+++ scenes/objects/rock/rock.gd
@@ -3,24 +3,29 @@ extends Sprite2D
 @onready var damage_component: DamageComponent = $DamageComponent
 @onready var hurt_component: HurtComponent = $HurtComponent
 
-var stone_scene = preload("res://scenes/objects/rock/middle_stone.tscn")
+var stone_scene = preload("res://scenes/objects/rock/small_stone.tscn")
+@export var stone_count: int = 1
 
-func  _ready() -> void:
+func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damaged_reached.connect(on_max_damage_reached)
-	
+
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
 	material.set_shader_parameter("shake_intensity", 0.5)
 	await get_tree().create_timer(0.5).timeout
 	material.set_shader_parameter("shake_intensity", 0.0)
-	
+
 func on_max_damage_reached() -> void:
 	call_deferred("add_stone_scene")
 	print("boom")
 	queue_free()
 
 func add_stone_scene() -> void:
-	var stone_instance = stone_scene.instantiate() as Node2D
-	stone_instance.global_position = global_position
-	get_parent().add_child(stone_instance)
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for i in range(stone_count):
+		var stone_instance = stone_scene.instantiate() as Node2D
+		var offset = Vector2(rng.randf_range(-20, 20), rng.randf_range(-10, 10))
+		stone_instance.global_position = global_position + offset
+		get_parent().add_child(stone_instance)
--- scenes/objects/tree/tree.gd
+++ scenes/objects/tree/tree.gd
@@ -3,6 +3,7 @@ extends Sprite2D
 @onready var damage_component: DamageComponent = $DamageComponent
 @onready var hurt_component: HurtComponent = $HurtComponent
 
+@export var log_count: int = 1
 var log_scene = preload("res://scenes/objects/tree/log.tscn")
 
 func  _ready() -> void:
@@ -21,6 +22,10 @@ func on_max_damage_reached() -> void:
 	queue_free()
 
 func add_log_scene() -> void:
-	var log_instance = log_scene.instantiate() as Node2D
-	log_instance.global_position = global_position
-	get_parent().add_child(log_instance)
+	var rng = RandomNumberGenerator.new()
+	rng.randomize()
+	for i in range(log_count):
+		var log_instance = log_scene.instantiate() as Node2D
+		var offset = Vector2(rng.randf_range(-20, 20), rng.randf_range(-10, 10))
+		log_instance.global_position = global_position + offset
+		get_parent().add_child(log_instance)
